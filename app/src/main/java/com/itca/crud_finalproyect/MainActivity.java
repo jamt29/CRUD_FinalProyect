3765833d19b126452329bf2990d06e550de6ac3a
Adony M <adony.torres29@gmail.com>
2021-11-03 10:33:35 -0600

    Diseño del activity_main (Login)

--- app/src/main/java/com/itca/crud_finalproyect/MainActivity.java
+++ app/src/main/java/com/itca/crud_finalproyect/MainActivity.java
@@ -2,6 +2,8 @@ package com.itca.crud_finalproyect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,10 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
     }
 
     public void btn(View view) {
         FancyToast.makeText(this,"Hello World !",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
 
+    }
+
+    public void onLoginClick(View view) {
+        //Descomentar despues
+        //startActivity(new Intent(this,RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
     }
 }
