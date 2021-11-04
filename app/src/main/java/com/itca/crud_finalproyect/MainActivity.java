b1ab21177ff7ec44e76980043879e482e7613129
Adony M <adony.torres29@gmail.com>
2021-11-03 20:14:25 -0600

    Eliminacion de la vista RegisterActivity, agregando la nueva vista registro_activity con su clase java

--- app/src/main/java/com/itca/crud_finalproyect/MainActivity.java
+++ app/src/main/java/com/itca/crud_finalproyect/MainActivity.java
@@ -14,20 +14,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+       if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
-
-        
-
+        setContentView(R.layout.activity_main);
     }
 
 
 
     public void onLoginClick(View view) {
 
-        startActivity(new Intent(this,RegisterActivity.class));
+        startActivity(new Intent(MainActivity.this, registro_activity.class));
         overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
     }
 }
--- app/src/main/java/com/itca/crud_finalproyect/RegisterActivity.java
+++
@@ -1,9 +0,0 @@
-package com.itca.crud_finalproyect;
-
-import android.app.Activity;
-import android.view.View;
-
-public class RegisterActivity extends Activity {
-    public void onLoginClick(View view) {
-    }
-}
